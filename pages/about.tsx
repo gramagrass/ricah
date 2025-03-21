b9bc1f7aa9d74cbc00275faf7b02aed5ca17088e
grama <grundum@gmail.com>
2025-03-20 20:11:52 -0500

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,30 @@
 // pages/about.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 
 const About: React.FC = () => {
+  const [randomImage, setRandomImage] = useState<string>('');
+
+  useEffect(() => {
+    // Generate a random number between 1 and 4
+    const randomNum = Math.floor(Math.random() * 4) + 1;
+    setRandomImage(`https://0fuqq7uksetsgrwn.public.blob.vercel-storage.com/ricah${randomNum}.jpg`);
+  }, []); // Empty dependency array ensures this runs only on initial render
+
   return (
     <div className="max-w-[1080px] mx-auto py-5 px-4 sm:px-6 lg:px-8">
       <Header />
-      <h1 className="text-2xl font-bold text-white mb-4">About Ricah</h1>
-      <p className="text-white">This is the about page for Ricah.</p>
+      <p className="text-white text-2xl">
+        RICAH es un proyecto de publicaciones independientes y colectivas compuestas con y desde RICAH, una fotocopiadora china e itinerante. Hoy desde Talleres TELECOM en el barrio Santa Fe de Bogotá. Para participar en los talleres y actividades relacionadas escriba a d@grama.co.
+      </p>
+      {randomImage && (
+        <img
+          src={randomImage}
+          alt="Random Ricah Image"
+          className="w-full max-w-[1080px] mx-auto h-auto object-contain mb-4 sm:w-screen sm:max-w-none"
+        />
+      )}
+
     </div>
   );
 };
